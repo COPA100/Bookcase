bfb172e18458cbbb1a5c28896bf9f1596314c103
COPA100 <colinpark100@gmail.com>
2025-08-12 23:57:18 -0400

    added add book functionality and dialog

---
+++ readme.md
@@ -0,0 +1,7 @@
+## About The Project
+This is an exercise found in the Odin Project, which is the course I am taking to solidify my knowledge in JS. I am taking it a step further and building a professional looking website.
+
+## Built With
+HTML, CSS, Tailwind, JS
+
+Color palette used: https://coolors.co/palette/dad7cd-a3b18a-588157-3a5a40-344e41
---
+++ src/app.js
@@ -0,0 +1,67 @@
+// DOM CODE DOM CODE DOM CODE DOM CODE DOM CODE DOM CODE DOM CODE DOM CODE DOM CODE DOM CODE DOM CODE
+const dialog = document.getElementById("bookDialog");
+const openBtn = document.getElementById("newBook");
+const closeBtn = document.getElementById("closeBtn");
+const submitBtn = document.getElementById("submitBtn");
+
+newBook.addEventListener("click", () => {
+  dialog.classList.add("animate-fade-up");
+  dialog.show();
+  setTimeout(() => {
+    dialog.classList.remove("animate-fade-up");
+  }, 500);
+});
+
+closeBtn.addEventListener("click", () => {
+  dialog.classList.add("animate-fade-out");
+  setTimeout(() => {
+    dialog.close();
+    dialog.classList.remove("animate-fade-out");
+  }, 100);
+});
+
+submitBtn.addEventListener("click", () => {
+  dialog.classList.add("animate-fade-out");
+  setTimeout(() => {
+    dialog.close();
+    dialog.classList.remove("animate-fade-out");
+  }, 100);
+});
+
+// DATA CODE DATA CODE DATA CODE DATA CODE DATA CODE DATA CODE DATA CODE DATA CODE DATA CODE DATA CODE
+const myLibrary = [];
+
+function Book(title, author, pages, read, id) {
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
+  this.read = read;
+  this.id = id;
+  this.info = () => {
+    return (
+      this.title +
+      " by " +
+      this.author +
+      ", " +
+      this.pages +
+      " pages, " +
+      this.read
+    );
+  };
+}
+
+function addBookToLibrary(title, author, pages, read) {
+  // take params, create a book then store it in the array
+  const id = crypto.randomUUID();
+  const newBook = new Book(title, author, pages, read, id);
+
+  myLibrary.push(newBook);
+}
+
+function displayLibrary() {
+  for (let i = 0; i < myLibrary.length; i++) {}
+}
+
+const harry = new Book("harry", "colin", "67", "not read yet");
+
+console.log(harry.info());
